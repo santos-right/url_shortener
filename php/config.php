32883bdacccf5cb8e4e15002a873d6f3b1534d5e
philip santos <santos.philip385@gmail.com>
2021-12-11 13:17:30 +0100

    removing database password

--- php/config.php
+++ php/config.php
@@ -2,7 +2,7 @@
 
     $domain = "localhost/url_shortener/";
 
-    $conn = mysqli_connect("localhost", "root", "santos", "urlshortener");
+    $conn = mysqli_connect("localhost", "root", "", "urlshortener");
     if(!$conn){ //if database is  not connected
         echo "Database connection error" .mysqli_connect_error();
     }
